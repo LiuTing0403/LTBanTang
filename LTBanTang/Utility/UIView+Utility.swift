c6ceb21d31709ad10051835020a3570e54128788
LiuTing0403 <liuting0403@foxmail.com>
2016-03-06 11:42:34 +0800

    add project

---
+++ LTBanTang/Utility/UIView+Utility.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Utility.swift
+//  LTBanTang
+//
+//  Created by liu ting on 16/3/6.
+//  Copyright © 2016年 liu ting. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func width()->CGFloat{
+        return self.bounds.width
+    }
+    
+    func height()->CGFloat {
+        return self.bounds.height
+    }
+    
+    func centerX()->CGFloat {
+        return self.center.x
+    }
+    
+    func centerY()->CGFloat {
+        return self.center.y
+    }
+}
